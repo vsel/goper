a0653b2c9a1d39f456951b23a32b1f2ed7b93956
vsel <vselyukh@gmail.com>
2021-12-06 22:35:28 +0200

    [new] add basic server from copypaste

--- main.go
+++ main.go
@@ -2,13 +2,93 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 	"strings"
 	"sync"
 	"time"
 )
 
 func main() {
+	logger := log.New(os.Stdout, "", 0)
+
+	hs := setup(logger)
+
+	logger.Printf("Listening on http://0.0.0.0%s\n", hs.Addr)
+
+	err := hs.ListenAndServe()
+	if err != nil {
+		logger.Printf("ListenAndServe error %s", err)
+	}
+}
+
+func setup(logger *log.Logger) *http.Server {
+	return &http.Server{
+		Addr:         getAddr(),
+		Handler:      newServer(logWith(logger)),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+}
+
+func getAddr() string {
+	if port := os.Getenv("PORT"); port != "" {
+		return ":" + port
+	}
+
+	return ":8383"
+}
+
+func newServer(options ...Option) *Server {
+	s := &Server{logger: log.New(ioutil.Discard, "", 0)}
+
+	for _, o := range options {
+		o(s)
+	}
+
+	s.mux = http.NewServeMux()
+	s.mux.HandleFunc("/", s.index)
+
+	return s
+}
+
+type Option func(*Server)
+
+func logWith(logger *log.Logger) Option {
+	return func(s *Server) {
+		s.logger = logger
+	}
+}
+
+type Server struct {
+	mux    *http.ServeMux
+	logger *log.Logger
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	s.log("%s %s", r.Method, r.URL.Path)
+
+	s.mux.ServeHTTP(w, r)
+}
+
+func (s *Server) log(format string, v ...interface{}) {
+	s.logger.Printf(format+"\n", v...)
+}
+
+func (s *Server) index(w http.ResponseWriter, _ *http.Request) {
+	_, err := w.Write([]byte("Hello, world!"))
+	if err != nil {
+		s.logger.Printf("ListenAndServe error %s", err)
+		return
+	}
 }
 
 func payloadWorker(
@@ -22,6 +102,7 @@ func payloadWorker(
 ) {
 	defer wg.Done()
 	limitTicker := time.NewTicker(repeatTimeout * time.Millisecond)
+	defer limitTicker.Stop()
 
 payloadLOOP:
 	for {
@@ -48,8 +129,7 @@ func sendPayload(
 		"POST", url, bodyReader,
 	)
 	if err != nil {
-		fmt.Println("NewRequest error")
-		fmt.Println(err)
+		fmt.Println("NewRequest error", err)
 		return
 	}
 	for headerKey, headerValue := range headers {
@@ -68,15 +148,13 @@ func makeRequest(client *http.Client, req *http.Request) {
 		}
 		err := resp.Body.Close()
 		if err != nil {
-			fmt.Println("Body close error")
-			fmt.Println(err)
+			fmt.Println("Body close error", err)
 			return
 		}
 	}(resp)
 
 	if err != nil {
-		fmt.Println("makeRequest error")
-		fmt.Println(err)
+		fmt.Println("makeRequest error", err)
 		return
 	}
 }
